05902cfc47a4190d8eac1ab846e80b4050490192
Anthony Reyes <reyesanthony8@gmail.com>
2022-08-01 21:35:25 -0400

    Rough css, home, about pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import Layout from "../components/layout";
+import * as React from "react";
+
+function AboutPage() {
+  return (
+    <main>
+      {" "}
+      <Layout pageTitle="Home Page" />
+      <title>About me</title>
+      <h1>About Me</h1> <p>This is just gonna be a little bit about me</p>
+    </main>
+  );
+}
+
+export default AboutPage;
